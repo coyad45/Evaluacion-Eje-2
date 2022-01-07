8c20a9021e476cbbddce8b22183b27037844cf96
coyad45 <coyad45@gmail.com>
2022-01-07 11:06:58 -0300

    Six Commit: Agregando la carpeta Biblioteca de clases

---
+++ BibliotecaDeClasesClinica/Clinica.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BibliotecaDeClasesClinica
+{
+    public class Clinica
+    {
+        public class Persona
+            {
+            public string Nombre { get; set; }
+            public string Apellido { get; set; }           
+             public string Dni { get; set; }
+             public string Edad { get; set; }
+             public string ObraSocial { get; set; }
+             public string NumeroDePaciente { get; set; }
+        }
+        public class Medico
+        {
+            public string Apellido { get; set; }
+            public string Nombre { get; set; }
+            public string Dni { get; set; }
+            public string Edad { get; set; }
+            public string Especialidad { get; set; }
+            public string Disponibilidad { get; set; }
+        }
+    }
+}
